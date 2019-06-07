04d255bb7d5c4ab672742ad34c694595cdcc4c6c
Rafael Rodriguez <rafael.roguz@gmail.com>
2019-06-07 15:34:26 -0500

    Refactorized code using routers and controllers

--- School/index.js
+++ School/index.js
@@ -1,76 +1,13 @@
 const express = require("express");
-const userServices = require("./services/user-services");
+const usersRouter = require("./routes/users-router");
+
 const app = express();
 
 app.use(express.json());
+app.use("/api/users/", usersRouter);
 
 app.get("/", (_req, res) => {
   res.send("<h1>Welcome to the School API!</h1>");
-});
-
-app.post("/api/users", (req, res) => {
-  userServices
-    .addUser(req.body)
-    .then(user => {
-      if (!user.upsertedCount) {
-        return res.status(400).send({});
-      }
-
-      res.status(200).send({
-        _id: user.upsertedId._id,
-        ...req.body
-      });
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
-
-app.get("/api/users", (_req, res) => {
-  userServices
-    .getAllUsers()
-    .then(users => {
-      res.send(users);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
-
-app.get("/api/users/:id", (req, res) => {
-  userServices
-    .getUser(req.params.id)
-    .then(user => {
-      res.send(user);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
-
-app.put("/api/users/:id", (req, res) => {
-  userServices
-    .updateUser(req.body)
-    .then(user => {
-      const result = user.modifiedCount ? req.body : {};
-      res.send(result);
-    })
-    .catch(err => {
-      res.status(500);
-      res.send(err);
-    });
-});
-
-app.delete("/api/users/:id", (req, res) => {
-  userServices
-    .deleteUser(req.params.id)
-    .then(result => {
-      const code = result.deletedCount ? 200 : 400;
-      res.status(code).send({});
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
 });
 
 const port = process.env.PORT || 3000;
